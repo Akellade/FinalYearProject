30c330ff05b210c06c5fc76d45b64f24e5ee6a40
Akellade <Aaronn.k@gmail.com>
2014-04-22 20:43:47 +0100

    Runs, opens blank window

--- Python/gui.py
+++ Python/gui.py
@@ -2,4 +2,13 @@ import Tkinter
 
 class simpleapp_tk(Tkinter.Tk): #inherit from Tkinter.Tk the base class for windows
 	def __init__(self,parent):
-		Tkinter.Tk.__init__(self,parent)/
+		Tkinter.Tk.__init__(self,parent)
+		self.parent = parent #reference to parent
+		self.initialise()
+		
+	def initialise(self): #method that will create everything like voidsetup etc
+		pass
+if __name__ == "__main__":
+	app = simpleapp_tk(None) # instantiate the class, parent = none
+	app.title('My Application') # window title
+	app.mainloop() # loop indefinitely
