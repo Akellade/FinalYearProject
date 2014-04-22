8781c2eabc6d5beb5d87cae20f460835ca137de9
Ak <Aaronn.k@gmail.com>
2014-04-22 11:04:30 +0100

    slowed down serial baud for debug. tested on scope etc. readting correctly. although power is not correct at this point.

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -8,8 +8,11 @@ Made for Arduino Uno
 int val = 0;
 int powerreq =0 ; // value received from pc for ideal power out.
 const float feedbackFactor = 0.009765624; //given by (5/1024)*2  or Vout/analogRead max * 2
-int resVal = 82
+int resVal = /*=1;//*/ 82;
 float Vout =0;
+float powerOut = 0 ;
+char inChar ;
+int stringComplete = 0;
 
 void setup(){
   // configure hardware timer2 to generate a fast PWM on OC2B (Arduino digital pin 3)
@@ -19,33 +22,44 @@ void setup(){
   OCR2A = 159;  // top/overflow value is 159 => produces a 100 kHz PWM
   pinMode(3, OUTPUT);  // enable the PWM output (you now have a PWM signal on digital pin 3)
   pinMode(A0, INPUT);
-  Serial.begin(9600);
+  Serial.begin(4800);
   
 }
 
 void loop()
 {
+  serialEvent();
+  
+  
  Vout = analogRead(A0) * (feedbackFactor);
  Serial.print("Vout:");
  Serial.print(Vout,3);
+ Serial.print("\t");
  
- powerOut = Vout * resVal; //calculate power
+ powerOut = (Vout * Vout) / resVal; //calculate power P=V^2/r
  
  //compare Actual power reading with the power requirement from system
 	Serial.print("Power Reading:");
-	Serial.print(powerOut);
+	Serial.print(powerOut,3);
 	Serial.print("Watts");
+        Serial.print("\n");
 	
 	
  if (powerOut > powerreq){ 		// if powerout too high 
 	OCR2B = --val;  			// decrease the PWM 
 	}
  if (powerOut < powerreq){ 		//if powerout too low
-	OCR2B = ++val;  			// increase the PWM 
+         OCR2B = ++val;			// increase the PWM 
 	}
  else{	 // else something? ?? not decided yet	
 	}
-	
+	Serial.print(OCR2B);
+        Serial.print("\t\n");
+
+if(OCR2B >159){
+         OCR2B = 159;
+        }
+        
 
 
 }
@@ -53,15 +67,18 @@ void loop()
 void serialEvent() { // repeats once, every time a loop completes. 
   while (Serial.available()) {
                     // get the new byte:
-                     powerreq = Serial.read()-'O'; // converts ASCII into int, since 'O' =48,
+                     powerreq = Serial.parseInt(); // converts ASCII into int, since 'O' =48,
+                     Serial.print("\t\t");
+                     Serial.print(powerreq);
+                     Serial.print("\n\n");
                     // add it to the inputString:
                     
                     // if the incoming character is a newline, set a flag
                     // so the main loop can do something about it:
                     if (inChar == '\n') {
-                                    stringComplete = true;
+                                    stringComplete = 1;
                                          } 
                    }
 }
 
-void printOutput 
+
